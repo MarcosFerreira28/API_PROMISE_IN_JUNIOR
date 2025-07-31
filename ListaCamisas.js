d2b74de47882ea21d3f5b2b7fd7f4e78a8074efa
Marcos <mvmendesferreira@id.uff.br>
2025-07-31 20:28:21 -0300

    add: adicionando chamada ao clicar em deletar do botão

--- ListaCamisas.js
+++ ListaCamisas.js
@@ -6,6 +6,22 @@ async function GetCamisas(limit, page){
         let produtos = await resposta.json();
         let quantidade = resposta.headers.get("x-total-count");
 
+        console.log(produtos);
+        console.log(quantidade);
+    }catch{
+        console.log(err => console.log(err))
+    }
+}
+
+
+async function DeletarCamisa(id) {
+    let url = `http://localhost:3000/products/${id}`;
+
+    try{
+        let deleta = await fetch( url, {
+            method: "DELETE",
+        })
+        console.log("deletado")
 
     }catch{
         console.log(err => console.log(err))
@@ -13,10 +29,18 @@ async function GetCamisas(limit, page){
 }
 
 
+// DeletarCamisa(1);
+// GetCamisas(8, 1);
+//FAZER PAGINAÇÃO COLOCANDO AS INFORMAÇÕES
 
+document.querySelectorAll('.deletar').forEach(botao => {
+    botao.addEventListener('click', function() {
+        const id = this.getAttribute('id');
+        DeletarCamisa(id);
+    });
+});
 
-GetCamisas(8, 1);
-
+//na hora de colcoar as informações nos cards colocar um id em cada botão
 
 
 
